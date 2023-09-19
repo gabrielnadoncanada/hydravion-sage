bee1ce315c21359db8986ac2931f87fdb14f072a
gabrielnadoncanada <gabrielnadoncanada@gmail.com>
2023-09-19 14:48:27 -0400

    fix

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -19,7 +19,10 @@
 					'menu_id' => 'footer-menu',
 					'menu_class' => 'text-white  leading-loose px-lg-10 mr-auto',
 				]) }}
-			<p class="leading-loose">2029, 15e avenue<br>St-Augustin-des-Desmaures<br>QC G3A 1W7</p>
+			<p class="leading-loose" style="font-style:normal;font-weight:400">2029, 15e avenue<br>St-Augustin-des-Desmaures<br>QC G3A 1W7</p>
+            <p class="leading-loose" style="font-style:normal;font-weight:400">Siège social : 2029, 15e avenue St-Augustin-des-Desmaures, QC G3A 1W7</p>
+            <p class="leading-loose" style="font-style:normal;font-weight:400">Base Mauricie : 30 Ch Contour-du-Lac-A-Beauce La Tuque, QC G9X 3N8</p>
+
 			<div>
 				<p class="leading-loose">
 					<a class="text-white hover:text-secondary" href="mailto:info@hydravionquebec.com">info@hydravionquebec.com</a>
